ad8ca1197f68dc1f09642c2456cfddc14f0d628a
Eric Ferguson <ericferguson@gmail.com>
2018-11-11 19:58:08 -0500

    Fixed buggy code

--- bin/cleanup-logs.zsh
+++ bin/cleanup-logs.zsh
@@ -6,21 +6,36 @@ CDATE=`date '+%Y%m%d'`
 
 cd $LOGDIR ;
 
-foreach i in `ls mws_*.txt 2>/dev/null`
+# Zip old files
+foreach i in `find . -name "mws_*.txt" 2>/dev/null | xargs`
 do
     # Get date and age of log
-    DATE=`echo $i | awk -F. '{print $2}'|awk -F_ '{print $1}'`
+    DATE=`echo $i | awk -F. '{print $3}'|awk -F_ '{print $1}'`
 
     D1=$(date -d "$CDATE" +%s)
     D2=$(date -d "$DATE" +%s)
     AGE=$(( ($D1 - $D2) / 86400 ))
-    # echo "file $DATE, age $AGE, $D1, $D2"
 
     # if date > $DAYSTOKEEP, zip it, else purge it
     if [[ $AGE -lt $DAYSTOKEEP ]] ; then
-        # echo "Zipping $i"
         tar -cvzf $i.tar.gz $i > /dev/null
     fi
     rm -f $i
+done
+
+# Delete old zip files
+foreach i in `find . -name "mws_*.tar.gz" 2>/dev/null | xargs`
+do
+    # Get date and age of log
+    DATE=`echo $i | awk -F. '{print $3}'|awk -F_ '{print $1}'`
+
+    D1=$(date -d "$CDATE" +%s)
+    D2=$(date -d "$DATE" +%s)
+    AGE=$(( ($D1 - $D2) / 86400 ))
+
+    # if date > $DAYSTOKEEP, zip it, else purge it
+    if [[ $AGE -gt $DAYSTOKEEP ]] ; then
+        rm -f $i
+    fi
 done
 
